f2c4a7ba7925f366c3a1d2988e846c25bdca3f10
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2017-02-06 12:50:33 +0100

    config filex

---
+++ mapview/collector/config.js
@@ -0,0 +1,9 @@
+const config = {
+  CASSANDRA_ENDPOINT: "localhost",
+  CASSANDRA_USER: "mapview_collector",
+  CASSANDRA_PASSWORD: "changeme",
+  LISTEN_ADDRESS: "localhost",
+  LISTEN_PORT: 3000,
+}
+
+module.exports = config;
