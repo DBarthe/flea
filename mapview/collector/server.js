42cbcfab3659cb0473214b6b2906a9b808d18953
Barthelemy Delemotte <barthelemy.delemotte@gmail.com>
2017-02-11 18:45:56 +0100

    update collector

--- mapview/collector/server.js
+++ mapview/collector/server.js
@@ -13,24 +13,40 @@ const authProvider = new cassandra.auth.PlainTextAuthProvider(config.CASSANDRA_U
 const client = new cassandra.Client({
   authProvider: authProvider,
   contactPoints: [config.CASSANDRA_ENDPOINT],
-  keyspace: 'mapview',
+  keyspace: 'fil',
 })
+
 client.connect(function (err){
   assert.ifError(err)
 })
-const reportInsert = "INSERT INTO report (user, ts, hostname) VALUES ( ?, dateOf(now()), ?) ;"
-const stationInsert = "INSERT INTO station (hostname, user, ts) VALUES (?, ?, dateOf(now()))"
+
+const reportByHostnameInsert = "INSERT INTO report (hostname, login, ts) VALUES ( ?, ?, dateOf(now())) ;"
+const reportByLoginInsert = "INSERT INTO report (login, hostname, ts) VALUES ( ?, ?, dateOf(now())) ;"
+const stationInsert = "INSERT INTO station (building, hostname, user, ts) VALUES ('M5', ?, dateOf(now()))"
 const studentInsert = "UPDATE student SET hostname=?, hostname_ts=dateOf(now()) WHERE login=?;"
 
 function collect(user, hostname, callback){
-  const reportParams = [ user, hostname ]
+
+  if (user == 'empty'){
+    user = null;
+  }
+
+  const reportByHostnameParams = [ hostname, user ]
+  const reportByLoginParams = [ user, hostname ]
   const stationParams = [ hostname, user ]
   const studentParams = [ hostname, user ]
-  return Promise.all([
-    client.execute(reportInsert, reportParams, { prepared: true }),
-    client.execute(stationInsert, stationParams, { prepared: true }),
-    client.execute(studentInsert, studentParams, { prepared: true })
-  ])
+
+  var queries = [
+    client.execute(reportByHostnameInsert, reportByHostnameParams, { prepared: true }),
+    client.execute(stationInsert, stationParams, { prepared: true })
+  ];
+
+  if (!user) {
+    queries.push(client.execute(reportByLoginInsert, reportByHostnameParams, { prepared: true }));
+    queries.push(client.execute(studentInsert, studentParams, { prepared: true }));
+  }
+
+  return Promise.all(queries);
 }
 
 server.route({
